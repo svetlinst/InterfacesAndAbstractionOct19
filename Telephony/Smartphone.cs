20010bd038483f03838304951e1fdcd4b86b7203
Svetlin Stoyanov <svetlinst@gmail.com>
2020-03-22 11:47:39 +0200

    Add project files.

---
+++ Telephony/Smartphone.cs
@@ -0,0 +1,28 @@
+﻿namespace Telephony
+{
+    using System;
+    using System.Linq;
+
+    public class Smartphone : ICallable, IBrowsable
+    {
+        public void Browse(string website)
+        {
+            if (website.Any(char.IsDigit))
+            {
+                Console.WriteLine($"Invalid URL!");
+                return;
+            }            
+            Console.WriteLine($"Browsing: {website}");
+        }
+
+        public void Call(string phoneNumber)
+        {
+            if (phoneNumber.Any(char.IsLetter))
+            {
+                Console.WriteLine($"Invalid number!");
+                return;
+            }
+            Console.WriteLine($"Calling... {phoneNumber}");
+        }
+    }
+}
---
+++ Telephony/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace Telephony
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            var addresses = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            Smartphone smartphone = new Smartphone();
+            foreach (var num in numbers)
+            {
+                smartphone.Call(num);
+            }
+            foreach (var address in addresses)
+            {
+                smartphone.Browse(address);
+            }
+        }
+    }
+}
